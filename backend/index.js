814e5a768c9a92256b01d12bc4b8492bc084fd95
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-05 19:43:44 +0200

    databse, env setup

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,27 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+const PORT = process.env.PORT || 7001;
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`ERROR: ${error}`);
+    });
