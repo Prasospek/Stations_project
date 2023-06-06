950f86a2d2be0e6f544c0a27ef5a15086683c2ff
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-07 01:36:50 +0200

    station

---
+++ backend/routes/admin.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+
+const router = express.Router();
+// check for role, authtoken? ?
+
+// TODO
+// Administrátor systému může spravovat uživatelské účty 
+// a zobrazovat statistiky o provozu a využívání metra na Marsu.
+
+// PATCH
+
+//router.patch("/:userId", getStations);
+
+
+
+export default router;
--- backend/routes/technician.js
+++ backend/routes/technician.js
@@ -2,7 +2,12 @@ import express from "express";
 import { getStations } from "../controllers/technician.js";
 
 const router = express.Router();
+// check for role, authtoken? ?
 
+// TODO
+// Technici mohou sledovat stav tratí a zadávat problémy a poruchy.
+
+/* GET*/ 
 router.get("/login", getStations);
 
 export default router;
