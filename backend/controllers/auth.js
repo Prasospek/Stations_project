8334c0cd9edf8521fa0c753ce5d66c68193fba09
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-06 17:08:59 +0200

    fix

---
+++ backend/controllers/auth.js
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    try {
+    } catch (err) {}
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+    try {
+    } catch (err) {}
+};
