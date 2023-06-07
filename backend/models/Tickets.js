dba3dd7802d9c4bc75dcddddc69794e2fb9f1125
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-07 15:03:00 +0200

    Ticket fix

--- backend/models/Tickets.js
+++ backend/models/Tickets.js
@@ -1,17 +1,37 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 // Definice schématu
-const ticketSchema = new mongoose.Schema({
-  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-  passenger_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  station_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Station', required: true },
-  purchase_method: { type: String, required: true },
-  destination_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Station', required: true },
-  purchase_date: { type: Date, default: Date.now }
-});
+const ticketSchema = new mongoose.Schema(
+    {
+        passenger_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        station_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Station",
+            required: true,
+        },
+        purchase_method: {
+            type: String,
+            required: true,
+        },
+        destination_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Station",
+            required: true,
+        },
+        purchase_date: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    { timestamps: true }
+);
 
 // Vytvoření modelu
-const Ticket = mongoose.model('Ticket', ticketSchema);
+const Ticket = mongoose.model("Ticket", ticketSchema);
 
 // Export modelu
 module.exports = Ticket;
