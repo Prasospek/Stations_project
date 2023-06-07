575189d2dd063092cc2de40baa3062a6a8c6a4bf
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-07 23:09:42 +0200

    Infoboard controllers

--- backend/controllers/infoBoards.js
+++ backend/controllers/infoBoards.js
@@ -1,12 +1,25 @@
-export const getInfoBoards = async (req, res) => {
+import InfoBoard from "../models/InfoBoard.js";
+
+export const getInfoBoard = async (req, res) => {
     try {
+        const { id } = req.params;
+        const infoBoard = await InfoBoard.findById(id);
+
+        if (!infoBoard) {
+            return res.status(404).json({ error: "InfoBoard not found !" });
+        }
+
+        res.status(200).json(infoBoard);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 };
 
-export const getInfoBoard = async (req, res) => {
+export const getInfoBoards = async (req, res) => {
     try {
+        const infoBoard = await InfoBoard.find();
+
+        res.status(200).json(infoBoard);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -14,6 +27,14 @@ export const getInfoBoard = async (req, res) => {
 
 export const createInfoBoard = async (req, res) => {
     try {
+        const { station_id, content } = req.body;
+
+        const newInfoBoard = new InfoBoard({
+            station_id,
+            content,
+        });
+
+        res.status(201).json(newInfoBoard);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -21,6 +42,19 @@ export const createInfoBoard = async (req, res) => {
 
 export const updateInfoBoard = async (req, res) => {
     try {
+        const { id } = req.params;
+        const { station_id, content } = req.body;
+
+        const updatedInfoBoard = await InfoBoard.findByIdAndUpdate(
+            id,
+            {
+                station_id,
+                content,
+            },
+            { new: true }
+        );
+
+        res.status(204).json(updatedInfoBoard);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
@@ -28,6 +62,11 @@ export const updateInfoBoard = async (req, res) => {
 
 export const deleteInfoBoard = async (req, res) => {
     try {
+        const { id } = req.params;
+
+        const deletedInfoBoard = await InfoBoard.findByIdAndDelete(id);
+
+        res.status(204).json(deletedInfoBoard);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
