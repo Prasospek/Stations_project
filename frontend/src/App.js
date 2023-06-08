b685f8bbffeee486b60d6dae0b329dfe6d8ef80a
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-09 00:40:08 +0200

    appjs fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,14 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./scenes/homePage";
 import LoginPage from "./scenes/loginPage";
 import { useSelector } from "react-redux";
+import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
 
 function App() {
     //const isAuth = Boolean(useSelector((state) => state.token));
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
     const isAuth = Boolean(
         useSelector((state) => {
@@ -18,14 +23,19 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LoginPage />} />
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route path="/" element={<LoginPage />} />
 
-                    <Route
-                        path="/home"
-                        element={isAuth ? <HomePage /> : <Navigate to="/" />}
-                    />
-                </Routes>
+                        <Route
+                            path="/home"
+                            element={
+                                isAuth ? <HomePage /> : <Navigate to="/" />
+                            }
+                        />
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     );
