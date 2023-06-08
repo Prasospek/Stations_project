dd2f1c05bb0cd49aeb01087d590960c036f397b3
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-08 23:44:20 +0200

    fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import { useSelector } from "react-redux";
 
 function App() {
-    return <div className="App"></div>;
+    const isAuth = Boolean(useSelector((state) => state.token));
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage />} />
+
+                    <Route
+                        path="/home"
+                        element={isAuth ? <HomePage /> : <Navigate to="/" />}
+                    />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
