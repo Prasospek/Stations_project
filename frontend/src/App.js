92fedd79129af43deb9c1156a809011c6472113b
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-08 23:58:03 +0200

    loggedout fixed routing problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,15 @@ import LoginPage from "./scenes/loginPage";
 import { useSelector } from "react-redux";
 
 function App() {
-    const isAuth = Boolean(useSelector((state) => state.token));
+    //const isAuth = Boolean(useSelector((state) => state.token));
+
+    const isAuth = Boolean(
+        useSelector((state) => {
+           //  console.log("state.token value:", state.token); // Add this line to log the value of state.token
+           // console.log("isAuth value:", isAuth);
+            return state.token;
+        })
+    );
 
     return (
         <div className="App">
--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+  return <div>LoginPage</div>;
 };
 
 export default LoginPage;
+
+/*
+import { useDispatch } from "react-redux";
+import { setLogout } from "../../state";
+
+function LogoutButton() {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        // Dispatch the setLogout action to clear the token
+        dispatch(setLogout());
+    };
+
+    return <button onClick={handleLogout}>Logout</button>;
+}
+
+export default LogoutButton;
+
+*/
