d3c4771cafcd2a0e7e4158de6d5a980458796337
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-09 01:44:30 +0200

    fix

--- frontend/src/scenes/loginPage/index.jsx
+++ frontend/src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setLogin } from "../../state/index";
-import FlexBetween from "components/FlexBetween";
+import FlexBetween from "../../components/FlexBetween";
 
 const registerSchema = yup.object().shape({
     firstName: yup.string().required("required"),
@@ -151,46 +151,7 @@ const Form = () => {
                                     border={`1px solid ${palette.neutral.medium}`}
                                     borderRadius="5px"
                                     p="1rem"
-                                >
-                                    <Dropzone
-                                        acceptedFiles=".jpg,.jpeg,.png"
-                                        multiple={false}
-                                        onDrop={(acceptedFiles) =>
-                                            setFieldValue(
-                                                "picture",
-                                                acceptedFiles[0]
-                                            )
-                                        }
-                                    >
-                                        {({ getRootProps, getInputProps }) => (
-                                            <Box
-                                                {...getRootProps()}
-                                                border={`2px dashed ${palette.primary.main}`}
-                                                p="1rem"
-                                                sx={{
-                                                    "&:hover": {
-                                                        cursor: "pointer",
-                                                    },
-                                                }}
-                                            >
-                                                <input {...getInputProps()} />
-                                                {!values.picture ? (
-                                                    <p>Add Picture Here</p>
-                                                ) : (
-                                                    <FlexBetween>
-                                                        <Typography>
-                                                            {
-                                                                values.picture
-                                                                    .name
-                                                            }
-                                                        </Typography>
-                                                        <EditOutlinedIcon />
-                                                    </FlexBetween>
-                                                )}
-                                            </Box>
-                                        )}
-                                    </Dropzone>
-                                </Box>
+                                ></Box>
                             </>
                         )}
 
