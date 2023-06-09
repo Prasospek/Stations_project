13dadf55d658fa04bfe2633c70d64a7036f26e25
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-10 01:52:36 +0200

    plannig

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -6,6 +6,10 @@ const HomePage = () => {
         <div>
             <Navbar />
             HomePage
+            <h3>If (user.role === "user) Bude tu 3 velky boxy a na vyber</h3>
+            <h4>Kupit lístek</h4>
+            <h4>Vsechny stanice</h4>
+            <h4>Zobrazit propojení stanic (img z drawio)</h4>
         </div>
     );
 };
