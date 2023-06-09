87fa43244e12c04c7b609929a865434afb083bd8
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-09 22:56:55 +0200

    log in form ?

--- frontend/src/scenes/loginPage/Form.jsx
+++ frontend/src/scenes/loginPage/Form.jsx
@@ -62,7 +62,27 @@ const Form = () => {
 
     const register = async (values, onSubmitProps) => {};
 
-    const login = async (values, onSubmitProps) => {};
+    const login = async (values, onSubmitProps) => {
+        const loggedInResponse = await fetch(
+            "http://localhost:8001/auth/login",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(values),
+            }
+        );
+        const loggedIn = await loggedInResponse.json();
+        onSubmitProps.resetForm();
+        if (loggedIn) {
+            dispatch(
+                setLogin({
+                    user: loggedIn.user,
+                    token: loggedIn.token,
+                })
+            );
+            navigate("/home");
+        }
+    };
 
     const handleFormSubmit = async (values, onSubmitProps) => {
         if (isLogin) {
