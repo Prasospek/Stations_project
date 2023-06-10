c4018859227b354fab6452713a4987b26e9c20d1
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-10 03:13:45 +0200

    user controller todo

---
+++ backend/controllers/users.js
@@ -0,0 +1,39 @@
+export const getUsers = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export const getUser = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export const getUsersTickets = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}; 
+
+export const updateUser = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
