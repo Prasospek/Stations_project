973414568df2a3068a2d75a631eeae2f7f6b233e
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-10 02:39:10 +0200

    fe

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,34 +1,68 @@
 import React from "react";
 import Navbar from "../navbar";
-import {
-    Box,
-    Button,
-    TextField,
-    useMediaQuery,
-    Typography,
-    useTheme,
-} from "@mui/material";
-import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
-import FlexBetween from "../../components/FlexBetween";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Box, useMediaQuery, useTheme } from "@mui/material";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
     const { palette } = useTheme();
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const isNonMobile = useMediaQuery("(min-width:800px)");
+    const isSmallScreen = useMediaQuery("(max-width:600px)");
+
     return (
         <div>
             <Navbar />
-            HomePage
-            <h3>If (user.role === "user) Bude tu X velkych boxi a na vyber</h3>
-            <h4>Kupit lístek</h4>
-            <h4>Moje lístky</h4>
-            <h4>Vsechny stanice</h4>
-            <h4>Zobrazit propojení stanic (img z drawio)</h4>
-            <h4>Dole random div s informace lorem + obrazek</h4>
+            <Box
+                display="grid"
+                gridTemplateColumns={
+                    isSmallScreen ? "1fr" : "repeat(2, minmax(0, 1fr))"
+                }
+                gap={2}
+                margin="2rem"
+            >
+                <Box
+                    bgcolor={palette.primary.main}
+                    color={palette.background.alt}
+                    padding="2rem"
+                    textAlign="center"
+                    borderRadius="5px"
+                    boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)"
+                >
+                    <h2>Buy ticket</h2>
+                </Box>
+                <Box
+                    bgcolor={palette.primary.main}
+                    color={palette.background.alt}
+                    padding="2rem"
+                    textAlign="center"
+                    borderRadius="5px"
+                    boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)"
+                >
+                    <h2>My tickets</h2>
+                </Box>
+                <Box
+                    bgcolor={palette.primary.main}
+                    color={palette.background.alt}
+                    padding="2rem"
+                    textAlign="center"
+                    borderRadius="5px"
+                    boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)"
+                >
+                    <h2>All Stations</h2>
+                </Box>
+                <Box
+                    bgcolor={palette.primary.main}
+                    color={palette.background.alt}
+                    padding="2rem"
+                    textAlign="center"
+                    borderRadius="5px"
+                    boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)"
+                >
+                    <h2>Display of routes linked to each Station</h2>
+                </Box>
+            </Box>
         </div>
     );
 };
