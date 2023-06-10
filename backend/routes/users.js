102480f60979045a663096e1b8ff91084a63fc3d
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-10 03:11:25 +0200

    be fix

---
+++ backend/routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const router = express.Router();
+
+/*GET */
+router.get("/", getUsers); // Ziskani vsech Usera
+router.get("/:id", getUser); // Ziskani specifické Usera
+router.get("/:id/tickets", getUsersTickets); // Ziskani vsech jizdenek Usera
+
+/* PUT */
+router.put("/:id", updateUser); // Upraveni detailu Usera
+
+/* DELETE */
+router.delete("/:id", deleteUser); // zrušení Usera
+
+export default router;
+
+// IMPORT MIDDELWARE JE TO ADMIN,TECHNICIAN?
