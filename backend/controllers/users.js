702de661f45b491e467efe134c62afdc432a8666
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-12 02:53:12 +0200

    be fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -96,7 +96,10 @@ export const updateUser = async (req, res) => {
             return res.status(404).json({ error: "User not found!" });
         }
 
-        res.status(200).json({ message: "User updated successfully" });
+        res.status(200).json({
+            message: "User updated successfully",
+            updatedUser,
+        });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
