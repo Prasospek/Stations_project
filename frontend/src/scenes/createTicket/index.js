49a98e5d9f2cb783088f7897f81444c4fcbb1d1a
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-13 17:24:59 +0200

    save

--- frontend/src/scenes/createTicket/index.js
+++ frontend/src/scenes/createTicket/index.js
@@ -18,12 +18,37 @@ import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const CreateTicketForm = () => {
-    const [purchaseMethod, setPurchaseMethod] = useState("");
-    const [stationId, setStationId] = useState("");
-    const [destinationId, setDestinationId] = useState("");
-    const [stations, setStations] = useState([]);
+const ticketSchema = yup.object().shape({
+    station_id: yup
+        .string()
+        .min(6, "Password must be at least 2 characters a-Z0-9")
+        .required("required and must be at least 2 characters a-Z0-9"),
+    purchase_method: yup
+        .string()
+        .oneOf(
+            ["Station", "Online"],
+            "Purchase Method must be either 'station' or 'online'"
+        )
+        .required("Purchase Method is required"),
+    destination_id: yup
+        .string()
+        .min(6, "Password must be at least 2 characters a-Z0-9")
+        .required("required and must be at least 2 characters a-Z0-9"),
+});
 
+const initialTicketValues = {
+    station_id: "",
+    purchase_method: "",
+    destination_id: "",
+};
+
+const CreateTicketForm = () => {
+    const { palette } = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("(min-width:800px)");
+
+    const [stations, setStations] = useState([]);
     const userId = useSelector((state) => state.user._id);
 
     useEffect(() => {
@@ -39,73 +64,101 @@ const CreateTicketForm = () => {
         }
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const handleTicketSubmit = async (values) => {
         try {
             const response = await axios.post("http://localhost:8001/tickets", {
                 passenger_id: userId,
-                station_id: stationId,
-                purchase_method: purchaseMethod,
-                destination_id: destinationId,
+                station_id: values.station_id,
+                purchase_method: values.purchase_method,
+                destination_id: values.destination_id,
             });
 
             console.log(response.data); // New ticket object
 
             // Reset form fields
-            setStationId("");
-            setPurchaseMethod("");
-            setDestinationId("");
+            values.station_id = "";
+            values.purchase_method = "";
+            values.destination_id = "";
+
+            toast.success("Ticket created successfully");
         } catch (error) {
             console.error("Error creating ticket:", error.message);
+            toast.error("Error creating ticket");
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <Select
-                label="Station"
-                name="station_id"
-                value={stationId}
-                onChange={(e) => setStationId(e.target.value)}
-                required
-                fullWidth
-            >
-                {stations.map((station) => (
-                    <MenuItem key={station._id} value={station._id}>
-                        {station.name}
-                    </MenuItem>
-                ))}
-            </Select>
-            <Select
-                label="Purchase Method"
-                name="purchase_method"
-                value={purchaseMethod}
-                onChange={(e) => setPurchaseMethod(e.target.value)}
-                required
-                fullWidth
-            >
-                <MenuItem value="Online">Online</MenuItem>
-                <MenuItem value="Station">Station</MenuItem>
-            </Select>
-            <Select
-                label="Destination"
-                name="destination_id"
-                value={destinationId}
-                onChange={(e) => setDestinationId(e.target.value)}
-                required
-                fullWidth
-            >
-                {stations.map((station) => (
-                    <MenuItem key={station._id} value={station._id}>
-                        {station.name}
-                    </MenuItem>
-                ))}
-            </Select>
-            <Button type="submit" variant="contained">
-                Create Ticket
-            </Button>
-        </form>
+        <Formik
+            initialValues={initialTicketValues}
+            validationSchema={ticketSchema}
+            onSubmit={handleTicketSubmit}
+        >
+            {(formik) => (
+                <form onSubmit={formik.handleTicketSubmit}>
+                    <Select
+                        label="Station"
+                        name="station_id"
+                        value={formik.values.station_id}
+                        onChange={formik.handleChange}
+                        error={
+                            formik.touched.station_id &&
+                            Boolean(formik.errors.station_id)
+                        }
+                        required
+                        fullWidth
+                    >
+                        {stations.map((station) => (
+                            <MenuItem key={station._id} value={station._id}>
+                                {station.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+
+                    <Select
+                        label="Purchase Method"
+                        name="purchase_method"
+                        value={formik.values.purchase_method}
+                        onChange={formik.handleChange}
+                        error={
+                            formik.touched.purchase_method &&
+                            Boolean(formik.errors.purchase_method)
+                        }
+                        required
+                        fullWidth
+                    >
+                        <MenuItem value="Online">Online</MenuItem>
+                        <MenuItem value="Station">Station</MenuItem>
+                    </Select>
+
+                    <Select
+                        label="Destination"
+                        name="destination_id"
+                        value={formik.values.destination_id}
+                        onChange={formik.handleChange}
+                        error={
+                            formik.touched.destination_id &&
+                            Boolean(formik.errors.destination_id)
+                        }
+                        required
+                        fullWidth
+                    >
+                        {stations.map((station) => (
+                            <MenuItem key={station._id} value={station._id}>
+                                {station.name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        disabled={formik.isSubmitting}
+                    >
+                        Create Ticket
+                    </Button>
+                </form>
+            )}
+        </Formik>
     );
 };
 
