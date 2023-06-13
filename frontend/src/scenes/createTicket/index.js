cc7c9dc1dec39e2ea92a7a4c18a1b17e66c85d79
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-13 17:53:09 +0200

    Buy ticket form correct

--- frontend/src/scenes/createTicket/index.js
+++ frontend/src/scenes/createTicket/index.js
@@ -1,39 +1,38 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Navbar from "../navbar";
+import Footer from "../footer/footer";
 import {
     TextField,
     Button,
     Select,
     MenuItem,
-    Box,
     useMediaQuery,
-    Typography,
     useTheme,
+    Box,
 } from "@mui/material";
-import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
 import * as yup from "yup";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const ticketSchema = yup.object().shape({
     station_id: yup
         .string()
-        .min(6, "Password must be at least 2 characters a-Z0-9")
-        .required("required and must be at least 2 characters a-Z0-9"),
+        .min(6, "Station ID must be at least 6 characters")
+        .required("Station ID is required"),
     purchase_method: yup
         .string()
         .oneOf(
             ["Station", "Online"],
-            "Purchase Method must be either 'station' or 'online'"
+            "Purchase Method must be either 'Station' or 'Online'"
         )
         .required("Purchase Method is required"),
     destination_id: yup
         .string()
-        .min(6, "Password must be at least 2 characters a-Z0-9")
-        .required("required and must be at least 2 characters a-Z0-9"),
+        .min(6, "Destination ID must be at least 6 characters")
+        .required("Destination ID is required"),
 });
 
 const initialTicketValues = {
@@ -44,8 +43,6 @@ const initialTicketValues = {
 
 const CreateTicketForm = () => {
     const { palette } = useTheme();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
     const isNonMobile = useMediaQuery("(min-width:800px)");
 
     const [stations, setStations] = useState([]);
@@ -64,7 +61,7 @@ const CreateTicketForm = () => {
         }
     };
 
-    const handleTicketSubmit = async (values) => {
+    const handleSubmit = async (values, { resetForm, setSubmitting }) => {
         try {
             const response = await axios.post("http://localhost:8001/tickets", {
                 passenger_id: userId,
@@ -76,89 +73,140 @@ const CreateTicketForm = () => {
             console.log(response.data); // New ticket object
 
             // Reset form fields
-            values.station_id = "";
-            values.purchase_method = "";
-            values.destination_id = "";
+            resetForm();
 
             toast.success("Ticket created successfully");
         } catch (error) {
             console.error("Error creating ticket:", error.message);
             toast.error("Error creating ticket");
+        } finally {
+            setSubmitting(false);
         }
     };
 
     return (
-        <Formik
-            initialValues={initialTicketValues}
-            validationSchema={ticketSchema}
-            onSubmit={handleTicketSubmit}
-        >
-            {(formik) => (
-                <form onSubmit={formik.handleTicketSubmit}>
-                    <Select
-                        label="Station"
-                        name="station_id"
-                        value={formik.values.station_id}
-                        onChange={formik.handleChange}
-                        error={
-                            formik.touched.station_id &&
-                            Boolean(formik.errors.station_id)
-                        }
-                        required
-                        fullWidth
+        <div>
+            <Navbar />
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="45vh"
+            >
+                <Box width="70%">
+                    <h2>Buy Ticket</h2>
+                    <Formik
+                        initialValues={initialTicketValues}
+                        validationSchema={ticketSchema}
+                        onSubmit={handleSubmit}
                     >
-                        {stations.map((station) => (
-                            <MenuItem key={station._id} value={station._id}>
-                                {station.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
+                        {({
+                            values,
+                            errors,
+                            touched,
+                            handleBlur,
+                            handleChange,
+                            handleSubmit,
+                            isSubmitting,
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                <Box
+                                    display="grid"
+                                    gap="30px"
+                                    gridTemplateColumns="repeat(2, minmax(0, 1fr))"
+                                    sx={{
+                                        "& > div": {
+                                            gridColumn: isNonMobile
+                                                ? undefined
+                                                : "span 2",
+                                        },
+                                    }}
+                                >
+                                    <Select
+                                        label="Station"
+                                        name="station_id"
+                                        value={values.station_id}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        error={
+                                            touched.station_id &&
+                                            Boolean(errors.station_id)
+                                        }
+                                        required
+                                        fullWidth
+                                    >
+                                        {stations.map((station) => (
+                                            <MenuItem
+                                                key={station._id}
+                                                value={station._id}
+                                            >
+                                                {station.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
 
-                    <Select
-                        label="Purchase Method"
-                        name="purchase_method"
-                        value={formik.values.purchase_method}
-                        onChange={formik.handleChange}
-                        error={
-                            formik.touched.purchase_method &&
-                            Boolean(formik.errors.purchase_method)
-                        }
-                        required
-                        fullWidth
-                    >
-                        <MenuItem value="Online">Online</MenuItem>
-                        <MenuItem value="Station">Station</MenuItem>
-                    </Select>
+                                    <Select
+                                        label="Purchase Method"
+                                        name="purchase_method"
+                                        value={values.purchase_method}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        error={
+                                            touched.purchase_method &&
+                                            Boolean(errors.purchase_method)
+                                        }
+                                        required
+                                        fullWidth
+                                    >
+                                        <MenuItem value="Online">
+                                            Online
+                                        </MenuItem>
+                                        <MenuItem value="Station">
+                                            Station
+                                        </MenuItem>
+                                    </Select>
 
-                    <Select
-                        label="Destination"
-                        name="destination_id"
-                        value={formik.values.destination_id}
-                        onChange={formik.handleChange}
-                        error={
-                            formik.touched.destination_id &&
-                            Boolean(formik.errors.destination_id)
-                        }
-                        required
-                        fullWidth
-                    >
-                        {stations.map((station) => (
-                            <MenuItem key={station._id} value={station._id}>
-                                {station.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
+                                    <Select
+                                        label="Destination"
+                                        name="destination_id"
+                                        value={values.destination_id}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        error={
+                                            touched.destination_id &&
+                                            Boolean(errors.destination_id)
+                                        }
+                                        required
+                                        fullWidth
+                                    >
+                                        {stations.map((station) => (
+                                            <MenuItem
+                                                key={station._id}
+                                                value={station._id}
+                                            >
+                                                {station.name}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </Box>
 
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        disabled={formik.isSubmitting}
-                    >
-                        Create Ticket
-                    </Button>
-                </form>
-            )}
-        </Formik>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    disabled={isSubmitting}
+                                    sx={{
+                                        marginTop: "1rem",
+                                    }}
+                                >
+                                    Create Ticket
+                                </Button>
+                            </form>
+                        )}
+                    </Formik>
+                </Box>
+            </Box>
+            <Footer />
+        </div>
     );
 };
 
