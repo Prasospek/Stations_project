b414e9077ec42aa7d75cf276e0de888d732a4b8e
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-14 01:08:01 +0200

    technician

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,42 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../navbar";
-import { Box, useMediaQuery, useTheme, Modal, Fade } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
 import Footer from "../footer/footer";
 import AdminPage from "../adminPage";
 import UserHomePage from "../userHomePage";
+import TechnicianHomePage from "../technicianHomePage";
 
 const HomePage = () => {
-    const { palette } = useTheme();
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const isNonMobile = useMediaQuery("(min-width:800px)");
-    const isSmallScreen = useMediaQuery("(max-width:600px)");
-    const user = useSelector((state) => state.user);
     const userRole = useSelector((state) => state.user.role);
-
-    const [showModal, setShowModal] = useState(false);
-
-    const handleStations = () => {
-        navigate("/stations");
-    };
-
-    const handleMyTickets = () => {
-        navigate(`/users/${user._id}/tickets`);
-    };
-
-    const handleBuyTicket = () => {
-        navigate(`/tickets`);
-    };
-
-    const handleDisplayRoutes = () => {
-        setShowModal(true);
-    };
-
-    const handleCloseModal = () => {
-        setShowModal(false);
-    };
 
     return (
         <div>
@@ -49,6 +21,12 @@ const HomePage = () => {
             {userRole === "admin" && (
                 <>
                     <AdminPage />
+                    <Box paddingBottom={"5rem"}></Box>
+                </>
+            )}
+            {userRole === "technician" && (
+                <>
+                    <TechnicianHomePage />
                     <Box paddingBottom={"5rem"}></Box>
                 </>
             )}
---
+++ frontend/src/scenes/technicianHomePage/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TechnicianHomePage = () => {
+    return <div>TechnicianHomePage</div>;
+};
+
+export default TechnicianHomePage;
