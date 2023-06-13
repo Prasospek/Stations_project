62e5849781aeaf44be0b9de2ac6de33b985ca874
Filip Stibor <75276590+Prasospek@users.noreply.github.com>
2023-06-13 19:44:44 +0200

    adminnpage work

--- frontend/src/scenes/adminPage/index.js
+++ frontend/src/scenes/adminPage/index.js
@@ -1,34 +1,56 @@
 import React, { useEffect, useState } from "react";
-import { Box, useMediaQuery, useTheme, Modal, Fade } from "@mui/material";
+import { Box, useTheme, IconButton } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AdminPage = () => {
     const { palette } = useTheme();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const isNonMobile = useMediaQuery("(min-width:800px)");
-    const isSmallScreen = useMediaQuery("(max-width:600px)");
     const user = useSelector((state) => state.user);
 
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const response = await fetch("http://localhost:8001/users");
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.error("Error fetching stations:", error);
-            }
-        };
+    const [users, setUsers] = useState([]);
 
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get("http://localhost:8001/users");
+            const data = response.data;
+            setUsers(data);
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+    };
+
+    useEffect(() => {
         fetchUsers();
     }, []);
 
-    const handleRemoveUser = (userId) => {
-        // Handle remove user logic here
-        // You can make an API call to delete the user
-        console.log(`Removing user with ID: ${userId}`);
+    const handleRemoveUser = async (userId) => {
+        const confirmed = window.confirm(
+            "Are you sure you want to delete this user?"
+        );
+
+        if (confirmed) {
+            try {
+                await axios.delete(`http://localhost:8001/users/${userId}`);
+                // Remove the deleted user from the users state
+                setUsers((prevUsers) =>
+                    prevUsers.filter((user) => user._id !== userId)
+                );
+                console.log(`Removing user with ID: ${userId}`);
+                toast.success(
+                    `${user.firstName} ${user.lastName} deleted successfully!`
+                );
+            } catch (error) {
+                console.error("Error removing user:", error);
+                toast.error("Error, there was a mistake!");
+            }
+        }
     };
 
     const handleUpdateUser = (userId) => {
@@ -37,7 +59,56 @@ const AdminPage = () => {
         console.log(`Updating user with ID: ${userId}`);
     };
 
-    return <div>AdminPage</div>;
+    return (
+        <div>
+            <ToastContainer />
+            {users.map((user) => (
+                <Box
+                    key={user._id}
+                    bgcolor={palette.primary.main}
+                    color={palette.background.alt}
+                    padding="1rem"
+                    marginBottom="1rem"
+                    borderRadius="5px"
+                    boxShadow="0 2px 5px rgba(0, 0, 0, 0.1)"
+                    display="flex"
+                    alignItems="flex-start"
+                    justifyContent="space-between"
+                    position="relative" // Add position relative for proper positioning
+                >
+                    <div>
+                        <h4>{`${user.firstName} ${user.lastName}`}</h4>
+                        <p>Email: {user.email}</p>
+                        <p>Password: {user.password}</p>
+                    </div>
+                    <div>
+                        <IconButton
+                            color="inherit"
+                            onClick={() => handleRemoveUser(user._id)}
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                right: 0,
+                            }}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                        <IconButton
+                            color="inherit"
+                            onClick={() => handleUpdateUser(user._id)}
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                right: "2rem",
+                            }}
+                        >
+                            <EditIcon />
+                        </IconButton>
+                    </div>
+                </Box>
+            ))}
+        </div>
+    );
 };
 
 export default AdminPage;
